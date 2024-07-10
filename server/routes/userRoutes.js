1cb9db7d41701eca4a37708e2fcc8687562485b0
arjun kshirsagar <kshirsagararjun20@gmail.com>
2024-07-10 10:22:59 +0530

    Routes Created Successfully

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,7 +4,14 @@ const User = require("../models/userModel");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+    try{
+        const newUser = new User(req.body);
+        await newUser.save();
 
+        res.status(201).json("User Created");
+    } catch (err) {
+        return res.status(500).json(err); 
+    }
 
 });
 
